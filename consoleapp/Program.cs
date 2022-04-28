65eb83414db1c6274ebdaa8d7220265edb648c9a
Alexander <bladyalexander333@gmail.com>
2022-04-28 18:55:06 -0500

    hello consoleapp

---
+++ consoleapp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Humanizer;
+
+Console.WriteLine("What's your name!");
+var name = Console.ReadLine();
+Console.WriteLine("Add your job!");
+var job = Console.ReadLine();
+Console.WriteLine("Add your age!");
+try
+{
+    var age = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Mi nombre es {name} y mi cargo es {job} y mi edad es {age.ToWords(new System.Globalization.CultureInfo("es"))},");
+}  catch (Exception)
+{
+    Console.WriteLine("Invalid age!");
+}
+
