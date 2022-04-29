6c732e91fcdf48eb616f62a2863abf28efd74bc6
Alexander <bladyalexander333@gmail.com>
2022-04-29 11:02:57 -0500

    PDOConcepts in c# with date

---
+++ PDOConcepts/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConcepsPDO;
+
+Console.WriteLine("PDO Concepts!");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2022, 3, 11));
+    Console.WriteLine(new Date(2022, 11, 31));
+}  catch (Exception error){
+    Console.WriteLine(error.Message);
+}
